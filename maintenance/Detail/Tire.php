d207c98b040bd25d4e87437dd2b993e9fa6554e1
jihun1844 <108657822+jihun1844@users.noreply.github.com>
2023-03-23 20:53:15 +0900

    Add files via upload

---
+++ maintenance/Detail/Tire.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    .box{
+      display: flex ;
+      justify-content: center;
+      align-items: center;
+      width: auto;
+      height: auto;
+      min-width: 100px;
+      min-height: 100px;
+      background-color: ;
+      border: 3px solid #000;
+    }
+    .title{
+       text-align: center;
+
+
+    }
+  </style>
+
+
+</head>
+<body>
+  <h2 class = "title">타이어</h2>
+  <div class = "box">
+   상세정보
+
+
+  </div>
+</body>
+</html>
